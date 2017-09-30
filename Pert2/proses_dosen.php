a39e139cae4d18b8f67748ad67a8f30a6355b27d
Carolina Ratna Sari <32198433+CarolinaRatnaS@users.noreply.github.com>
2017-09-30 12:56:44 +0700

    COMMIT Update Dosen

--- Pert2/proses_dosen.php
+++ Pert2/proses_dosen.php
@@ -5,7 +5,13 @@
 		// 2. query
 		$query = "INSERT INTO dosen (kodeDosen, nama)
 				  VALUES('$_POST[kodeDosen]', '$_POST[nama]')";
-	}
+	}else if($_GET['action'] == "edit") {
+		// 2. query
+		$query = "UPDATE dosen
+				  SET kodeDosen = '$_POST[kodeDosen]',
+					  nama = '$_POST[nama]'
+				  WHERE id = $_POST[id]";
+	} 
 	
 	mysqli_query($koneksi, $query);
 	
