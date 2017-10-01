66c9c6bfab4cedd3bb9655321a1e68cbfe3261f6
Carolina Ratna Sari <32198433+CarolinaRatnaS@users.noreply.github.com>
2017-10-01 15:31:47 +0700

    COMMIT Update Mata Kuliah

--- Pert2/proses_matkul.php
+++ Pert2/proses_matkul.php
@@ -5,6 +5,12 @@
 		// 2. query
 		$query = "INSERT INTO matkul (kodeMatkul, namaMatkul)
 				  VALUES('$_POST[kodeMatkul]', '$_POST[namaMatkul]')";
+	} else if($_GET['action'] == "edit") {
+		// 2. query
+		$query = "UPDATE matkul
+				  SET kodeMatkul = '$_POST[kodeMatkul]',
+					  namaMatkul = '$_POST[namaMatkul]'
+				  WHERE id = $_POST[id]";
 	}
 	
 	mysqli_query($koneksi, $query);
@@ -12,5 +18,3 @@
 	// REDIRECT ke template.php
 	header('Location: template.php?page=matkul');
 ?>
-
-
