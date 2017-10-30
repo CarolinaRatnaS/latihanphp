f430873f975cae19359a55891c613b3634d05f6b
Carolina Ratna Sari <32198433+CarolinaRatnaS@users.noreply.github.com>
2017-10-30 12:51:29 +0700

    COMMIT - Menambah pages di navigasi subjects

--- pert6/public/manage_content.php
+++ pert6/public/manage_content.php
@@ -1,30 +1,53 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>	
 
-<?php
-	// 2. Perform database query
-	$query = "SELECT * FROM subjects WHERE visible = 1 ORDER BY position ASC";
-	$result = mysqli_query($connection, $query);
-	confirm_query($result);
-?>
-
 <?php include("../includes/layouts/header.php"); ?>		
 
 	<div id="main">
 		<div id="navigation">
 			<ul class="subjects">
 			<?php
+				<?php
+				// 2. Perform database query
+				$query = "SELECT * FROM subjects WHERE visible = 1 ORDER BY position ASC";
+				$subject_set = mysqli_query($connection, $query);
+				confirm_query($page_set);
+				?>
+			
 				// 3. Use returned data (if any)
-				while($row = mysqli_fetch_row($result)) {
-					// oitput dat from each row
-					//var_dump($row);
-					//echo "<hr />";
-				//}
+				while($row = mysqli_fetch_row($subject_set)) {
+					
 			?>
-			<li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
+			<li>
+				<?php echo $subject["menu_name"]; ?>
+				<?php
+				// 2. Perform database query
+				$query = "SELECT * FROM pages 
+						  WHERE visible = 1 
+						  AND subject_id = {$subject["id"]} 
+						  ORDER BY position ASC";
+				$page_set = mysqli_query($connection, $query);
+				confirm_query($page_set);
+				?>
+				
+				<ul class="pages">
+					<?php
+						while($page_set = mysqli_fetch_assoc($page_set)) {
+					?>
+						<li><?php echo $page["menu_name"]; ?></li>
+					<?php
+						}
+					?>
+					<?php mysqli_free_result($page_set);?>
+				</ul>
+			</li>
 			<?php
 				}
 			?>
+			<?php mysqli_free_result($subject_set);?>
+			
+			
+			
 		</ul> 
 		</div>
 		<div id="page">
@@ -32,9 +55,5 @@
 			
 		</div>
 	</div>
-	<?php
-		// 4. Release returned data
-		mysqli_free_result($result);
-	?>
 
 <?php include("../includes/layouts/footer.php"); ?>
